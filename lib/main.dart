83dbd5b259e4ca34bf98a85eed0fa66694f5672f
NdegwaJulius <juliusndegwa2@gmail.com>
2022-10-26 07:05:24 +0300

    Setting the bottom bar navigation

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:foodly/fooderlich_theme.dart';
+import 'package:foodly/home.dart';
+
+void main() {
+  runApp(const Fooderlich());
+}
+
+class Fooderlich extends StatelessWidget {
+  const Fooderlich({super.key});
+  @override
+  Widget build(BuildContext context) {
+    final theme = FooderlichTheme.dark();
+    return MaterialApp(
+      theme: theme,
+      title: 'Fooderlich',
+      home: const Home(),
+    );
+  }
+}
