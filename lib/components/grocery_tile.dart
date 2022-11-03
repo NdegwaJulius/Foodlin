8035da15ece45fb1d7acfa074b5e133e3a61fe96
NdegwaJulius <juliusndegwa2@gmail.com>
2022-11-03 11:51:18 +0300

    Displaying the selected date

--- lib/components/grocery_tile.dart
+++ lib/components/grocery_tile.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 
 import '../models/grocery_item.dart';
 
@@ -57,6 +58,15 @@ class GroceryTile extends StatelessWidget {
       throw Exception('This importance type does not exist');
     }
   }
+
 // TODO: Add buildDate()
+  Widget buildDate() {
+    final dateFormatter = DateFormat('MMMM dd h:mm a');
+    final dateString = dateFormatter.format(item.date);
+    return Text(
+      dateString,
+      style: TextStyle(decoration: textDecoration),
+    );
+  }
 // TODO: Add buildCheckbox()
 }
